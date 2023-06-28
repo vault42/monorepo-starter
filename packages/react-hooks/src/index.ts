74af78d486702c8f93c874e80492943e71d36197
42arch <rend42@163.com>
2023-06-28 10:10:39 +0800

    chore: format

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -1,33 +1,33 @@
 'use client'
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 const storage = {
   key: 'last-visit-date',
   save: (value: string) => window.localStorage.setItem(storage.key, value),
-  retrieve: () => window.localStorage.getItem(storage.key) ?? null,
+  retrieve: () => window.localStorage.getItem(storage.key) ?? null
 }
 
 export const useLastVisitDate = () => {
-  const [lastVisitDate, setLastVisitDate] = useState<null | string>(null);
+  const [lastVisitDate, setLastVisitDate] = useState<null | string>(null)
 
   // On page load retrieve the last visit date if exists
   useEffect(() => {
-    const value = storage.retrieve();
-    setLastVisitDate(value);
-  }, []);
+    const value = storage.retrieve()
+    setLastVisitDate(value)
+  }, [])
 
   // On page close save current date
   useEffect(() => {
     const handler = () => {
-      const currentDate = new Date().toISOString();
-      storage.save(currentDate);
+      const currentDate = new Date().toISOString()
+      storage.save(currentDate)
     }
-    window.addEventListener('beforeunload', handler);
+    window.addEventListener('beforeunload', handler)
     return () => {
-      window.removeEventListener('beforeunload', handler);
+      window.removeEventListener('beforeunload', handler)
     }
-  }, []);
+  }, [])
 
-  return lastVisitDate;
+  return lastVisitDate
 }
