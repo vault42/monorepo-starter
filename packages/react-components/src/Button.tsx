70446157d27741bcb39d01a68d06fcf6ca466843
42arch <rend42@163.com>
2023-07-04 21:35:55 +0800

    feat(react-components): add click event in button

--- packages/react-components/src/Button.tsx
+++ packages/react-components/src/Button.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 
 export interface ButtonProps {
+  onClick: () => void
   children: React.ReactNode
 }
 
 export function Button(props: ButtonProps) {
-  return <button>{props.children}</button>
+  return <button onClick={props.onClick}>{props.children}</button>
 }
 
 Button.displayName = 'Button'
